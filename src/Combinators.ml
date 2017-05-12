d7c22c8fbbe96377ba002186a75b68fc8bca3972
kverty <danila_yumsh@mail.ru>
2017-05-12 18:53:53 +0300

    Meerkat way to do seq and alt

--- src/Combinators.ml
+++ src/Combinators.ml
@@ -2,15 +2,15 @@
  * Ostap: basic set of parser combinators.
  * Copyright (C) 2006-2008
  * Dmitri Boulytchev, St.Petersburg State University
- * 
+ *
  * This software is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License version 2, as published by the Free Software Foundation.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
+ *
  * See the GNU Library General Public License version 2 for more details
  * (enclosed in the file COPYING).
  *)
@@ -18,7 +18,70 @@
 open Printf
 open Matcher
 open List
+open Hashtbl
 open Types
+
+(*new*)
+
+let input = "something"
+
+let success i = function k -> (k i)
+let failure i = function k -> ((*do nothing*))
+
+let cpsterminal c =
+  function i -> (
+    if input.[i] = c
+    then success (i + 1)
+    else failure i)
+
+let cpsepsilon =
+  function i ->
+    success i
+
+let cpsseq a b =
+  function i ->
+    (function k ->
+      a i (function t -> b t k))
+
+let cpsalt a b =
+  function i ->
+    (function k -> begin a i k; b i k end)
+
+let memoresult res =
+  let rs = ref [] in
+  let ks = ref [] in
+    function k -> (
+      if (List.length !ks = 0)
+      then begin
+        ks := k::!ks;
+        let ki = function t -> (if (not (List.mem t !rs))
+                                then begin
+                                  rs := t::!rs;
+                                  let f elem = elem t in
+                                  List.iter f !ks
+                                end) in
+        res () ki
+      end
+      else begin
+        ks := k::!ks;
+        List.iter k !rs
+      end)
+
+let memo f =
+  let table = Hashtbl.create 16 in
+    function i -> (
+      if (Hashtbl.mem table i)
+      then begin
+        Hashtbl.find table i
+      end
+      else begin
+        add table i (memoresult (fun () -> f i));
+        Hashtbl.find table i
+      end
+    )
+
+(*old*)
+
 
 let join = function
 | None   -> fun y -> y
@@ -31,7 +94,7 @@ let comment str = function
 let return x = (fun s -> Parsed ((x, s), None))
 let cast     = function Failed x -> Failed x | _ -> invalid_arg "Ostap.cast"
 
-let map f p s = 
+let map f p s =
   match p s with
   | Parsed ((b, s'), e) -> Parsed ((f b, s'), e)
   | x -> cast x
@@ -41,36 +104,36 @@ let (-->) p f = map f p
 let empty s   = Parsed (((), s), None)
 let fail  r s = Failed r
 let lift  s   = Parsed ((s, s), None)
-let sink  p s = 
+let sink  p s =
   match p s with
   | Parsed ((s, _), f) -> Parsed ((s, s), f)
   | Failed x           -> Failed x
 
 let alt x y s =
-  match x s with 
-  | Failed x ->      
-      (match y s with 
-      | Failed y -> Failed (join x y) 
+  match x s with
+  | Failed x ->
+      (match y s with
+      | Failed y -> Failed (join x y)
       | Parsed (ok, err) -> Parsed (ok, join x err)
-      )     
+      )
   | x -> x
-    
+
 let (<|>) = alt
 
 let seq x y s =
   match x s with
-  | Parsed ((b, s'), err) ->	
-      (match y b s' with 
-      | Failed  x     -> Failed (join err x) 
+  | Parsed ((b, s'), err) ->
+      (match y b s' with
+      | Failed  x     -> Failed (join err x)
       | Parsed (s, e) -> Parsed (s, join err e)
-      )	
+      )
   | x -> cast x
-    
+
 let (|>) = seq
 
 let opt p s =
-  match p s with 
-  | Parsed ((x, s'), d) -> Parsed ((Some x, s'), d) 
+  match p s with
+  | Parsed ((x, s'), d) -> Parsed ((Some x, s'), d)
   | Failed d            -> Parsed ((None, s), d)
 
 let (<?>) = opt
@@ -81,9 +144,9 @@ let manyFold f init p =
     | Parsed ((x, s'), d) -> inner (join err d) (f acc x) s'
     | Failed d            -> Parsed ((acc, s), join err d)
   in
-  inner None init 
+  inner None init
 
-let many p = 
+let many p =
   (manyFold (fun acc x -> fun l -> acc (x::l)) (fun x -> x) p) --> (fun t -> t [])
 
 let (<*>) = many
@@ -93,12 +156,12 @@ let someFold f init p = p |> (fun h -> manyFold f (f init h) p)
 let some p = (someFold (fun acc x -> fun l -> acc (x::l)) (fun x -> x) p) --> (fun t -> t [])
 
 let (<+>) = some
-    
-let guard p f r s = 
+
+let guard p f r s =
   match p s with
-  | (Parsed ((b, _), _) as x) -> 
-      if f b 
-      then x 
+  | (Parsed ((b, _), _) as x) ->
+      if f b
+      then x
       else Failed (match r with None -> None | Some r -> Some (r b))
   | y -> y
 
@@ -113,56 +176,56 @@ let unwrap r f g =
   match r with
   | Parsed ((x, _), _) -> f x
   | Failed x           -> g x
-  
-class memoStream s = 
+
+class memoStream s =
   object (this : 'self)
     inherit Matcher.t s
-    
+
     val table : ((int * int) * int) list = []
 
-    method memoize : 'p 'e . ('self, 'p, 'e) parse -> ('self, 'p, 'e) result = 
-      fun p -> 
+    method memoize : 'p 'e . ('self, 'p, 'e) parse -> ('self, 'p, 'e) result =
+      fun p ->
         let getParsedValue t p pos =
           let equal (f0, p0) (f1, p1) = f0 == f1 && p0 = p1 in
-          let rec find key tab = 
-            match tab with 
+          let rec find key tab =
+            match tab with
             | [] -> raise Not_found
-            | (k, v) :: t -> if equal key k 
+            | (k, v) :: t -> if equal key k
                              then v
                              else find key t
           in
           Obj.magic (find (Obj.magic p, pos) t)
         in
         let replaceValue t p pos v =
-          ((Obj.magic p, pos), (Obj.magic v)) :: t 
+          ((Obj.magic p, pos), (Obj.magic v)) :: t
         in
         let rec increaseBound t p pos =
           match p {< table = t >} with
-          | Failed _ -> getParsedValue t p this#pos            
-          | Parsed ((_, s), _) as parsed -> 
-            if s#pos > pos 
+          | Failed _ -> getParsedValue t p this#pos
+          | Parsed ((_, s), _) as parsed ->
+            if s#pos > pos
             then increaseBound (replaceValue t p this#pos parsed) p s#pos
             else getParsedValue t p this#pos
         in
-        try 
-          getParsedValue table p this#pos           
-        with  
-          Not_found -> 
+        try
+          getParsedValue table p this#pos
+        with
+          Not_found ->
             match p {< table = replaceValue table p this#pos (Failed None) >} with
             | Failed _ as r -> r
             | Parsed ((b, s'), e) as r ->
               increaseBound (replaceValue table p this#pos r) p this#pos
   end
-  
-let memo = 
+
+let memo =
   fun p (s : #memoStream) -> s#memoize p
 
-let fix p s = 
-  let x' = ref None in  
+let fix p s =
+  let x' = ref None in
   let rec fix p s = p (fix p) s in
-  let p x s = 
+  let p x s =
     match !x' with
-    | None   -> x' := Some (fun s -> memo x s); p x s 
-    | Some x -> p x s 
+    | None   -> x' := Some (fun s -> memo x s); p x s
+    | Some x -> p x s
   in
   fix p s
